9e56e41333662684150ffa643a9eab2c41dcffd3
changelia <changelia97@gmail.com>
2021-05-03 10:10:35 +0400

    feat: fetch posts

---
+++ src/hooks/useGetPosts.ts
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { getAllPostSuccess } from 'pages/posts/postActins/actions'
+import service from 'services/service'
+
+interface PostInterface {
+  body: string
+  title: string
+  userId: string
+}
+
+export const useGetPosts = () => {
+  const [loading, setLoading] = useState(false)
+  const dispatch = useDispatch()
+
+  const getPosts = async () => {
+    setLoading(true)
+
+    const data = await service.getAll('posts')
+
+    dispatch(getAllPostSuccess(data.data))
+
+    setLoading(false)
+  }
+
+  return {
+    getPosts,
+    loading
+  }
+}
