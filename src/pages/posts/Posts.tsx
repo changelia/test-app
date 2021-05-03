1f6e14635da296b263aa3d7bd7469685882a0e8f
changelia <changelia97@gmail.com>
2021-05-04 00:03:03 +0400

    fix: post page interface

--- src/pages/posts/Posts.tsx
+++ src/pages/posts/Posts.tsx
@@ -1,17 +1,19 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useCallback, useMemo } from 'react'
 import { useSelector } from 'react-redux'
 import moment from 'moment'
-import { useHistory, useParams } from 'react-router-dom'
-import { Comment, Tooltip } from 'antd'
+import { useHistory } from 'react-router-dom'
+import { Comment, Tooltip, Avatar } from 'antd'
 import { CommentOutlined } from '@ant-design/icons'
 import { useGetPosts } from 'hooks/useGetPosts'
 import Button from 'components/button'
+import { Input } from 'components/input'
 import AddPostModal from 'components/addPostModal'
 import styled from 'styled-components'
 
-const Posts = (props: any) => {
+const Posts = () => {
   const history = useHistory()
   const [open, setOpens] = useState(false)
+  const [searchText, setSearchText] = useState('')
   const { getPosts } = useGetPosts()
 
   useEffect(() => {
@@ -19,6 +21,11 @@ const Posts = (props: any) => {
   }, [])
 
   const data = useSelector((state: any) => state.posts)
+
+  const handleChange = useCallback((e: any) => {
+    const { value } = e.currentTarget
+    setSearchText(value)
+  }, [searchText])
 
   const onClick = (id: string) => history.push(`/posts/${id}/comments`)
 
@@ -30,21 +37,40 @@ const Posts = (props: any) => {
     </Tooltip>,
   ])
 
+  const filteredData = useMemo(() =>
+    data && data.postList.filter((i: any) =>
+      i.body.toLocaleLowerCase().includes(searchText.toLocaleLowerCase())), [searchText, data])
+
   return (
     <>
-      <Button
-        text="Create Post"
-        type="primary"
-        onClick={() => setOpens(true)}
-      />
+      <StyledWrapper>
+        <Button
+          text="Create Post"
+          type="primary"
+          onClick={() => setOpens(true)}
+        />
+
+        <Input
+          value={searchText}
+          onChange={handleChange}
+          placeholder="Search..."
+          name="search"
+        />
+      </StyledWrapper>
       <StyledContainer>
-        {data && data.postList.map((i: any) => (
+        {filteredData.map((i: any) => (
           <Comment
             actions={actions(i)}
             author={<a>{i.title}</a>}
             key={i.id}
+            avatar={
+              <Avatar
+                src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
+                alt="Han Solo"
+              />
+            }
             content={
-              <p>
+              <p onClick={() => onClick(i.id)} style={{ cursor: 'pointer' }}>
                 {i.body}
               </p>
             }
@@ -67,4 +93,9 @@ export default Posts
 const StyledContainer = styled.div`
   height: 70vh;
   overflow-y: overlay;
+`
+const StyledWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 120px 260px auto;
+  grid-column-gap: 20px;
 `
