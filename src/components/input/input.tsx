31d9187eb40ff322d0b442a8d1dd7eff2f0b6e19
changelia <changelia97@gmail.com>
2021-05-03 10:13:28 +0400

    fix: input props

--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -4,13 +4,15 @@ interface Props {
   placeholder: string
   onChange: (e?: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>) => void
   value: string
+  name: string
 }
 
-export default ({ placeholder, onChange, value }: Props) => (
+export default ({ placeholder, onChange, value, name }: Props) => (
   <Input
     placeholder={placeholder}
     onChange={onChange}
     value={value}
+    name={name}
   />
 )
 
