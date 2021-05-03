57c0dd46e0e46f2ae3a8b0eac09c3988939e8a94
changelia <changelia97@gmail.com>
2021-05-03 22:14:33 +0400

    fix: add comment reducer

--- src/pages/comments/commentReducer.ts
+++ src/pages/comments/commentReducer.ts
@@ -8,8 +8,17 @@ export const commentReducer = (state = initialState, action: any) => {
   switch (action.type) {
 
     case actionTypes.GET_COMMENTS_BY_POST:
-
       return { commentList: { ...state.commentList, [action.postId]: action.data } }
+
+    case actionTypes.ADD_COMMENT_SUCCESS:
+      const { commentList }: any = state
+
+      return {
+        commentList: {
+          ...state.commentList,
+          [action.postId]: [action.data, ...commentList[action.postId]]
+        }
+      }
 
     default:
       return state
